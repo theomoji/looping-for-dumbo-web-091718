4b6f569b843107e9746c216f521145d47c79cf3f
Shuo Wang <gken90@gmail.com>
2018-08-25 19:20:25 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,15 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+	for checklist
+		puts "Wingardium Leviosa"
 end
 
 
+# Fill out the content of the using_for method in the for.rb file so that it puts "Wingardium Leviosa" ten (10) times, once for each item on our checklist.
+
+# shoe_count = 1..32
+#
+# for shoe in shoe_count
+#     puts "I'm putting a pair of shoes away."
+# end
