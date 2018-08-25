f9debf6fd0564c7b26918e39b91e0c2afd67ca27
Shuo Wang <gken90@gmail.com>
2018-08-25 19:21:53 +0000

    automatically backed up by learn

--- for.rb
+++ for.rb
@@ -2,7 +2,7 @@
 def using_for
 	checklist = 1..10
 	for checklist do
-		puts "Wingardium Leviosa"
+		puts "puts until the phrase 'Wingardium Leviosa' 10 times, once for each item on the checklist"
   end
 end
 
